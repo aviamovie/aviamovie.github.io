048075061521804df4bcd1d6df7b065438ee3f9e
aviamovie <roman.yaroslavsky@gmail.com>
2026-05-01 20:19:33 +0700

    Update surs_nav_buttons.js

--- v3/surs_nav_buttons.js
+++ v3/surs_nav_buttons.js
@@ -216,13 +216,13 @@ function addStyles() {
             }  
             .card__button-label {  
                 position: absolute;  
-                bottom: 0.2em;  
+                bottom: 0.1em;  
                 left: 0;  
                 right: 0;  
                 text-align: center;  
                 color: #fff;  
                 padding: 0.4em;  
-                font-size: 0.5em;  
+                font-size: 0.4em;  
                 font-weight: 300;  
                 z-index: 1;  
             }  
