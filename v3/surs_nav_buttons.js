a73f9699da737927daf1a38c7f763e808f6d451a
aviamovie <roman.yaroslavsky@gmail.com>
2026-05-01 20:06:09 +0700

    Update surs_nav_buttons.js

--- v3/surs_nav_buttons.js
+++ v3/surs_nav_buttons.js
@@ -188,102 +188,12 @@ function getExternalButtons() {
 }
       
     // Добавление стилей с мобильными адаптациями  
-function addStyles() {      
-    Lampa.Template.add('custom_buttons_compact_style', `      
-        <style>      
-            .card--button-compact {      
-                width: 12.75em !important;      
-            }      
-            .items-line {      
-                padding-bottom: 0.5em !important;      
-            }      
-    
-            @media screen and (max-width: 767px) {      
-                .card--button-compact {      
-                    width: 9em !important;      
-                }      
-           
-                .items-line {      
-                    padding-bottom: 0.3em !important;      
-                }      
-                  
-                .card__svg-icon {      
-                    position: absolute;      
-                    top: 37%;      
-                    left: 50%;      
-                    transform: translate(-50%, -50%);      
-                    width: 40% !important;      
-                    height: 40% !important;      
-                    display: flex;      
-                    align-items: center;      
-                    justify-content: center;        
-                }     
-                    .card__button-label {      
-                    position: absolute;      
-                    bottom: 0.2em;      
-                    left: 0;      
-                    right: 0;      
-                    text-align: center;      
-                    color: #fff;      
-                    padding: 0.4em;      
-                    font-size: 0.5em;      
-                    font-weight: 300;      
-                    z-index: 1;      
-                }      
-            }      
-    
-            .card--button-compact .card__view {      
-                padding-bottom: 56% !important;      
-                display: flex;      
-                align-items: center;      
-                justify-content: center;      
-                background-color: rgba(0, 0, 0, 0.2);      
-                border-radius: 1em;      
-            }      
-            .card--button-compact.hover .card__view,      
-            .card--button-compact.focus .card__view {      
-                background-color: rgba(255, 255, 255, 0.1);      
-            }      
-            .card--button-compact .card__title,      
-            .card--button-compact .card__age {      
-                display: none !important;      
-            }      
-            .card__svg-icon {      
-                position: absolute;      
-                top: 45%;      
-                left: 50%;      
-                transform: translate(-50%, -50%);      
-                width: 40% !important;      
-                height: 40% !important;      
-                display: flex;      
-                align-items: center;      
-                justify-content: center;      
-            }      
-            .card__svg-icon svg {      
-                width: 100% !important;      
-                height: 100% !important;      
-                //fill: currentColor;  
-            }      
-
-            .card__svg-icon svg[fill="none"],  
-            .card__svg-icon svg[fill="transparent"] {  
-                fill: transparent !important;  
-            }
-            .card__button-label {      
-                position: absolute;      
-                bottom: 0.4em;      
-                left: 0;      
-                right: 0;      
-                text-align: center;      
-                color: #fff;      
-                padding: 0.5em;      
-                font-size: 1.0em;      
-                font-weight: 400;      
-                z-index: 1;      
-            }      
-        </style>      
-    `);      
-    $('body').append(Lampa.Template.get('custom_buttons_compact_style', {}, true));      
+function addStyles() {  
+    if (!window.surs_custom_css_loaded) {  
+        Utils.putScript(['https://aviamovie.github.io/surs_custom_css.js'], function() {  
+            console.log('SURS Custom CSS loaded');  
+        });  
+    }  
 }
       
     function createCard(data, type) {  
